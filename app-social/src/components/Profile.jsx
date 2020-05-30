c2cd0cd6d7bc2141eeebcd4fc571a91919a2343d
alexey <tozikalexey@gmail.com>
2020-05-30 14:50:27 +0300

    Add: basic components

---
+++ app-social/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <img src="https://images2.alphacoders.com/902/902946.png"></img>
+            <div>ava + desc</div>
+            <div>
+                <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Profile;
